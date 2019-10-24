bd076b0a7de6dba0b279d1e5844dbc8d555f81a7
chenfan <chenfan@skytech.cn>
2019-10-24 13:32:21 +0800

    add:swagger 版本 2.8.0 ,remarks:之后的pom要统一管理版本

---
+++ src/main/java/cn/pre/cf/SwaggerProperties.java
@@ -0,0 +1,25 @@
+package cn.pre.cf;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Dora
+ * @date 2019/10/23 15:16
+ **/
+@Data
+@ConfigurationProperties(
+        prefix = "swagger"
+)
+@Component
+public class SwaggerProperties {
+
+    private String title;
+
+    private String description;
+
+    private String version;
+
+    private String scanPackage;
+}
